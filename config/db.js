b5fb97141c7a50e9c41f0f135516ae35db863999
TurunenP <k7jepu00@students.oamk.fi>
2023-01-12 17:15:41 +0300

    added config file db.js with a method to connect to mongodb cluster

---
+++ config/db.js
@@ -0,0 +1,32 @@
+/**
+ * This file is used to configure the database connection.
+ */
+
+// load env file
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const MONGODB_URL = "mongodb+srv://doadmin:3Cn2Kp4jcJ80W659@db-mongodb-nyc1-22526-ccc29d80.mongo.ondigitalocean.com/admin?retryWrites=true&w=majority"
+
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+       
+
+        });
+        console.log('MongoDB Connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+    };
+
+
+module.exports = connectDB;
+
