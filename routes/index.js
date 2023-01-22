43a0ca77f1e97a024ef733c24af3c8479c3b709b
TurunenP <k7jepu00@students.oamk.fi>
2023-01-22 17:02:41 +0300

    added station routes and modified journey routes

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,33 @@ const router = express.Router();
 
 // Import the controllers
 
-const { getJourneys} = require('../controllers/journeyController');
+const { getJourneys,addjourney,getJourney} = require('../controllers/journeyController');
 
 
 
-
+// Journey routes
 router.get('/journeys', getJourneys);
+
+router.get('/journeys/:id', getJourney);
+
+router.post('/journeys', addjourney);
+
+// Station routes
+
+const { getStations,addStation,getStation,updateStation} = require('../controllers/stationController');
+
+router.get('/stations', getStations);
+
+router.get('/stations/:id', getStation);
+
+router.post('/stations', addStation);
+
+router.put('/stations/:id', updateStation);
+
+
+
+
+
 
 
 
